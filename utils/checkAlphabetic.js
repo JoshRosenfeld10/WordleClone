6b3d4af4f2cd9708345b553f2e0aa30d7a62a21a
Josh Rosenfeld <joshr@rogers.com>
2024-04-19 17:32:18 -0400

    changed condition for valid alphabetic entry

--- utils/checkAlphabetic.js
+++ utils/checkAlphabetic.js
@@ -1,12 +1,14 @@
 const checkAlphabetic = (input) => {
-    if (input === 'DELETE' || input === 'META') return false
+    if (input.length !== 1 && input !== 'ENTER' && input !== 'BACKSPACE') return false
+
     for (const char of input) {
         if (!(char >= "a" && char <= "z") &&
             !(char >= "A" && char <= "Z")) {
             return false;
         }
-    }
+
     return true;
+    }
 }
 
 export default checkAlphabetic;
