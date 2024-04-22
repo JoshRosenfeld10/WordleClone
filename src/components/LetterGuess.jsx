20e7fbdd91ec2d986f7312bb6acbd5dc90b0f386
Josh Rosenfeld <joshr@rogers.com>
2024-04-22 11:55:40 -0400

    updated media queries

--- src/components/LetterGuess.jsx
+++ src/components/LetterGuess.jsx
@@ -3,10 +3,10 @@ letterState = green, yellow, grey, blank
 */
 
 const letterClasses = {
-  green: "greenLetter sm:w-14 sm:h-14 w-12 h-12 text-white font-bold sm:text-4xl text-3xl  flex justify-center items-center cursor-default letter-reveal",
-  yellow: "yellowLetter sm:w-14 sm:h-14 w-12 h-12 text-white font-bold sm:text-4xl text-3xl flex justify-center items-center cursor-default letter-reveal",
-  grey: "greyLetter sm:w-14 sm:h-14 w-12 h-12 text-white font-bold sm:text-4xl text-3xl flex justify-center items-center cursor-default letter-reveal",
-  blank: "border-2 border-[#3a3a3c] sm:w-14 sm:h-14 w-12 h-12 text-white font-bold sm:text-4xl text-3xl flex justify-center items-center cursor-default"
+  green: "greenLetter w-14 h-14 text-white font-bold text-4xl  flex justify-center items-center cursor-default letter-reveal",
+  yellow: "yellowLetter w-14 h-14 text-white font-bold text-4xl flex justify-center items-center cursor-default letter-reveal",
+  grey: "greyLetter w-14 h-14 text-white font-bold text-4xl flex justify-center items-center cursor-default letter-reveal",
+  blank: "border-2 border-[#3a3a3c] w-14 h-14 text-white font-bold text-4xl flex justify-center items-center cursor-default"
 }
 
 function LetterGuess({ letter, letterState }) {
