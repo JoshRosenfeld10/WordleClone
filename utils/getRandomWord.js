a2c19b7d51dcd0fba43c1bc7bc69269f286e0348
Josh Rosenfeld <joshr@rogers.com>
2026-02-17 20:57:28 -0500

    updated random word api

--- utils/getRandomWord.js
+++ utils/getRandomWord.js
@@ -1,10 +1,12 @@
-import axios from "axios"
+import axios from "axios";
 
 const getWord = async () => {
-   const response = await axios.get('https://random-word-api.vercel.app/api?words=1&length=5');
-   const data = await response.data
+  const response = await axios.get(
+    "https://random-word-api.herokuapp.com/word?length=5",
+  );
+  const data = await response.data;
 
-   return data[0]
-}
+  return data[0];
+};
 
 export default getWord;
